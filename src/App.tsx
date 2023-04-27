917c10fa2b530ae395cef211e7c28361db0f54d2
Sotatek-JustinLe <thanh.le2@sotatek.com>
2023-04-28 01:34:46 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Iris</h1>
+        <h1>Hello, Iris</h1>
         <h1>env: {process.env.REACT_APP_HELLO}</h1>
         <h1>env 2: {process.env.REACT_APP_HELLO_2}</h1>
         <img src={logo} className="App-logo" alt="logo" />
